1c3eef2de1ab656da72042fc96b8af348a3dcf99
Judy <judy@qq.com>
2022-09-11 19:42:55 +0800

    关注，取关，共同关注功能的实现

--- src/main/java/com/hmdp/controller/FollowController.java
+++ src/main/java/com/hmdp/controller/FollowController.java
@@ -1,9 +1,12 @@
 package com.hmdp.controller;
 
 
-import org.springframework.web.bind.annotation.RequestMapping;
+import com.hmdp.dto.Result;
+import com.hmdp.service.IFollowService;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.*;
 
-import org.springframework.web.bind.annotation.RestController;
+import javax.annotation.Resource;
 
 /**
  * <p>
@@ -16,5 +19,22 @@ import org.springframework.web.bind.annotation.RestController;
 @RestController
 @RequestMapping("/follow")
 public class FollowController {
+    @Autowired
+    private IFollowService followService;
 
+    @GetMapping("/common/{id}")
+    public Result followCommoms(@PathVariable Long id){
+        return followService.followCommons(id);
+    }
+
+
+    @PutMapping("/{id}/{isFollow}")
+    public Result follow(@PathVariable("id") Long id,@PathVariable("isFollow") Boolean isFollow){
+        return followService.follow(id,isFollow);
+    }
+
+    @GetMapping("/or/not/{id}")
+    public Result followOrNot(@PathVariable Long id){
+        return followService.followOrNot(id);
+    }
 }
